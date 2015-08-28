df491f1250e1e3d363fc08488a20c0e1c7f0b3de
Derrick J Chie <djchie.dev@gmail.com>
2015-08-28 14:08:48 -0700

    Setup the skeleton of the game

--- client/watchout.js
+++ client/watchout.js
@@ -1,5 +1,7 @@
 // start slingin' some d3 here.
 
+/* SETUP THE ENVIRONMENT */
+
 var gameOptions = {
   height: 450,
   width: 700,
@@ -12,26 +14,20 @@ var gameStats = {
   bestScore: 0
 };
 
+/* SETUP THE GAME BOARD */
+
+// Axes
 var axes = {
   x: d3.scale.linear().domain([0, 100]).range([0, gameOptions.width]),
   y: d3.scale.linear().domain([0, 100]).range([0, gameOptions.height])
 };
 
+// Game Board (svg region)
 var gameBoard = d3.select('.container').append('svg:svg')
             .attr('width', gameOptions.width)
             .attr('height', gameOptions.height);
 
-console.log(gameBoard);
-
-d3.select('.container > svg').append('svg');
-var incrX = 0;
-var incrY = 0;
-for (var i = 0; i < gameOptions.nEnemies; i++) {
-  incrX += 12;
-  incrY += 12;
-  d3.selectAll('.container > svg > svg').append('circle').attr('cx', incrX).attr('cy', incrY).attr('r', '10');
-}
-
+// Scores
 var updateScore = function() {
   d3.select('#current-score').text(gameStats.score.toString());
 };
@@ -43,6 +39,50 @@ var updateBestScore = function() {
   }
 };
 
+/* THE PLAYER */
+
+/* ENEMIES */
+
+var createEnemies = function() {
+  var enemies = [];
+  for (var i = 0; i < gameOptions.nEnemies; i++) {
+    enemies.push({
+      id: i,
+      x: (Math.random() * 100),
+      y: (Math.random() * 100)
+    })
+  }
+  return enemies;
+}
+
+/* RENDERING THE GAME BOARD */
+
+/* PLAY THE GAME! */
+
+var play = function() {
+  var gameTurn = function() {
+    newEnemyPositions = createEnemies();
+    render(newEnemyPositions);
+  }
+  
+}
+
 
 //SVGs covering a larger area than just the circle
 //
+
+
+
+
+
+
+console.log(gameBoard);
+
+d3.select('.container > svg').append('svg');
+var incrX = 0;
+var incrY = 0;
+for (var i = 0; i < gameOptions.nEnemies; i++) {
+  incrX += 12;
+  incrY += 12;
+  d3.selectAll('.container > svg > svg').append('circle').attr('cx', incrX).attr('cy', incrY).attr('r', '10');
+}
