a23932be6f65f93f45b764f3017a9de85ce85c1e
djchie <djchie.dev@gmail.com>
2015-08-28 16:13:58 -0700

    Implemented the transitions of the enemy circles

--- client/watchout.js
+++ client/watchout.js
@@ -41,6 +41,8 @@ var updateBestScore = function() {
 
 /* THE PLAYER */
 
+
+
 /* ENEMIES */
 
 var createEnemies = function() {
@@ -53,9 +55,64 @@ var createEnemies = function() {
     })
   }
   return enemies;
-}
+};
 
 /* RENDERING THE GAME BOARD */
+
+var render = function(enemy_data) {
+  var enemies = gameBoard.selectAll('circle.enemy')
+    .data(enemy_data, function(d) { return d.id;});
+    // debugger;
+  enemies.enter()
+    .append('svg:circle')
+      .attr('class', 'enemy')
+      .attr('cx', function(enemy) { return axes.x(enemy.x); })
+      .attr('cy', function(enemy) { return axes.y(enemy.y); })
+      .attr('r', 10);
+
+    enemies.exit()
+      .remove();
+
+    // checkCollision
+    // onCollision
+    
+    var tweenWithCollisionDetection = function(endData) {
+      
+      var enemy = d3.select(this);
+      
+      var startPos = {
+        x: parseFloat(enemy.attr('cx')),
+        y: parseFloat(enemy.attr('cy'))
+      };
+
+      var endPos = {
+        x: axes.x(endData.x),
+        y: axes.y(endData.y),
+      };
+
+      return function(t) {
+        // checkCollision(enemy, onCollision);
+
+        var enemyNextPos = {
+          x: (startPos.x + (endPos.x - startPos.x) * t),
+          y: (startPos.y + (endPos.y - startPos.y) * t)
+        };
+
+        return enemy.attr('cx', enemyNextPos.x)
+          .attr('cy', enemyNextPos.y);
+      };
+
+    };
+
+  return enemies.transition()
+    .duration(500)
+    .attr('r', 10)
+  .transition()
+    .duration(2000)
+    .tween('custom', tweenWithCollisionDetection);
+};
+
+
 
 /* PLAY THE GAME! */
 
@@ -64,8 +121,18 @@ var play = function() {
     newEnemyPositions = createEnemies();
     render(newEnemyPositions);
   }
-  
+
+  var increaseScore = function() {
+    gameStats.score++;
+    updateScore();
+  }
+
+  gameTurn();
+  setInterval(gameTurn, 2000);
+  setInterval(increaseScore, 50);
 }
+
+play();
 
 
 //SVGs covering a larger area than just the circle
@@ -76,13 +143,13 @@ var play = function() {
 
 
 
-console.log(gameBoard);
+// console.log(gameBoard);
 
-d3.select('.container > svg').append('svg');
-var incrX = 0;
-var incrY = 0;
-for (var i = 0; i < gameOptions.nEnemies; i++) {
-  incrX += 12;
-  incrY += 12;
-  d3.selectAll('.container > svg > svg').append('circle').attr('cx', incrX).attr('cy', incrY).attr('r', '10');
-}
+// d3.select('.container > svg').append('svg');
+// var incrX = 0;
+// var incrY = 0;
+// for (var i = 0; i < gameOptions.nEnemies; i++) {
+//   incrX += 12;
+//   incrY += 12;
+//   d3.selectAll('.container > svg > svg').append('circle').attr('cx', incrX).attr('cy', incrY).attr('r', '10');
+// }
